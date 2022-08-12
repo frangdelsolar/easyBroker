0863a8574c2b49fa0c379e6995859343446122a3
Francisco Javier González del Solar <frangdelsolar@gmail.com>
2022-08-12 20:31:26 -0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PropertiesDetailComponent } from './pages/properties-detail/properties-detail.component';
+import { PropertiesListComponent } from './pages/properties-list/properties-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PropertiesListComponent
+  },
+  {
+    path: ':id',
+    component: PropertiesDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
